9525f718cc847723e8a142cd48e3b659e2197b7c
ccrawford4 <calumcrawford9@gmail.com>
2025-03-23 13:38:37 -0700

    using outputs from ec2 module

--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -1,3 +1,7 @@
 output "ec2_dns_endpoint" {
   value = aws_instance.bastion.public_dns
 }
+
+output "ec2_private_ip" {
+  value = aws_instance.private_ec2[*].private_ip
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "ec2_dns_endpoint" {
   value = module.ec2.ec2_dns_endpoint
 }
+
+output "ec2_private_ip" {
+  value = module.ec2.ec2_private_ip
+}
