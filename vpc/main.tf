589c0b08b7bf2591788c139735db4e38a04d2be6
ccrawford4 <calumcrawford9@gmail.com>
2025-03-23 20:27:08 -0700

    remove oneflow

--- vpc/main.tf
+++ vpc/main.tf
@@ -8,60 +8,56 @@ data "aws_availability_zones" "available" {
 }
 
 # Create the VPC
-resource "aws_vpc" "oneflow_vpc" {
+resource "aws_vpc" "vpc" {
   cidr_block = var.vpc_cidr_block
   enable_dns_hostnames = true 
 }
 
 # Create an Internet Gateway
-resource "aws_internet_gateway" "oneflow_igw" {
-  vpc_id = aws_vpc.oneflow_vpc.id
-
-  tags = {
-    Name = "oneflow_igw"
-  }
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws.vpc.vpc.id
 }
 
 # Create a public subnet
-resource "aws_subnet" "oneflow_public_subnet" {
+resource "aws_subnet" "public_subnet" {
   count = var.subnet_count.public
-  vpc_id = aws_vpc.oneflow_vpc.id
+  vpc_id = aws_vpc.vpc.id
   cidr_block = var.public_subnet_cidr_blocks[count.index]
   availability_zone = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
 }
 
 # Create the private subnet
-resource "aws_subnet" "oneflow_private_subnet" {
+resource "aws_subnet" "_private_subnet" {
   count = var.subnet_count.private
-  vpc_id = aws_vpc.oneflow_vpc.id
+  vpc_id = aws_vpc.vpc.id
   cidr_block = var.private_subnet_cidr_blocks[count.index]
   availability_zone = data.aws_availability_zones.available.names[count.index]
 }
 
 # Create a public route table
-resource "aws_route_table" "oneflow_public_rt" {
-  vpc_id = aws_vpc.oneflow_vpc.id
+resource "aws_route_table" "_public_rt" {
+  vpc_id = aws_vpc.vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.oneflow_igw.id
+    gateway_id = aws_internet_gateway._igw.id
   }
 }
 
 # Create the private route table
-resource "aws_route_table" "oneflow_private_rt" {
-  vpc_id = aws_vpc.oneflow_vpc.id
+resource "aws_route_table" "_private_rt" {
+  vpc_id = aws_vpc.vpc.id
 }
 
 # Associate the public route table with the public subnets
 resource "aws_route_table_association" "public" {
   count          = var.subnet_count.public
-  route_table_id = aws_route_table.oneflow_public_rt.id
-  subnet_id      = aws_subnet.oneflow_public_subnet[count.index].id
+  route_table_id = aws_route_table.public_rt.id
+  subnet_id      = aws_subnet.public_subnet[count.index].id
 }
 
 resource "aws_route_table_association" "private" {
   count          = var.subnet_count.private
-  route_table_id = aws_route_table.oneflow_private_rt.id
-  subnet_id      = aws_subnet.oneflow_private_subnet[count.index].id
+  route_table_id = aws_route_table.private_rt.id
+  subnet_id      = aws_subnet.private_subnet[count.index].id
 }
--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,11 +1,11 @@
 output "vpc_id" {
-  value = aws_vpc.oneflow_vpc.id 
+  value = aws_vpc.vpc.id 
 }
 
 output "public_subnet_id" {
-  value = aws_subnet.oneflow_public_subnet[0].id
+  value = aws_subnet.public_subnet[0].id
 }
 
 output "private_subnet_id" {
-  value = aws_subnet.oneflow_private_subnet[0].id
+  value = aws_subnet.private_subnet[0].id
 }
